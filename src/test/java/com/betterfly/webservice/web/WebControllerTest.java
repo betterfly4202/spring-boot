868a746b85bcd10a2b075d2f37dc95dd538384f5
betterfly <betterfly4202@gmail.com>
2018-01-27 18:54:24 +0900

    - 커밋 재시도

---
+++ src/test/java/com/betterfly/webservice/web/WebControllerTest.java
@@ -0,0 +1,33 @@
+package com.betterfly.webservice.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+
+/**
+ * Created by betterFLY on 2018. 1. 27.
+ * Github : http://github.com/betterfly88
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩(){
+        //When
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //Then
+        assertThat(body).contains("스프링 부트를 사용해서 웹을 구축해 봅니다.");
+    }
+}
